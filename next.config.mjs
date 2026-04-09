14565f4d3276ca3d1922f509da38e362391b125a
Simon Edwards <simon@onedotsix.com>
2026-04-09 12:28:05 +0200

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+    images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cms.thebench.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn-api.swapcard.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'static.swapcard.com',
+			}
+		],
+	}
 };
 
 export default nextConfig;
